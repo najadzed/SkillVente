4f740c82f62a7d77198ad1140a99ccae7415fe31
Najad <najaddh@gmail.com>
2025-09-19 16:40:57 +0530

    Update settings.py

--- skillvente/settings.py
+++ skillvente/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +89,7 @@ DATABASES = {
         },
     }
 }
-import os
+
 # Media files (uploads)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
